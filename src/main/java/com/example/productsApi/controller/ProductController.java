f406315a565c314ffdd2e0ea3837a7111430cef3
Deivison Resende <deivison4844@gmail.com>
2025-03-15 18:58:21 -0300

    feat: inject Products repository in controller

--- src/main/java/com/example/productsApi/controller/ProductController.java
+++ src/main/java/com/example/productsApi/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.example.productsApi.controller;
 
 import com.example.productsApi.model.Product;
+import com.example.productsApi.repository.ProductRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("products")
 public class ProductController {
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     @PostMapping()
     public void create(@RequestBody Product product) {
